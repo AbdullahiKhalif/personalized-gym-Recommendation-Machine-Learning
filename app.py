ac33b999909c807e1ea78fb10dbb330032303d40
Abdullahi Khalif <abdullajust2021@gmai.com>
2024-04-29 11:52:07 +0300

    Added: login, signupp page and connected mysql datatbase

--- app.py
+++ app.py
@@ -1,27 +1,117 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify, session, redirect, url_for
 from joblib import load
-
+import mysql.connector
+import re
 app = Flask(__name__)
 
 # Load your models
 decision_tree_regression_model = load('models/decision_tree_regressor.joblib')
-decision_tree_classification__model = load('models/decision_tree_classifier.joblib')
+decision_tree_classification_model = load('models/decision_tree_classifier.joblib')
+
+app = Flask(__name__)
+app.secret_key = "xtay6UY&"
+
+# MySQL configurations
+mysql_config = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': '',
+    'database': 'gym_recommendation'
+}
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def index():
+    if 'username' in session:
+        return render_template('index.html')
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        conn = mysql.connector.connect(**mysql_config)
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM users WHERE email = %s AND password = %s", (email, password))
+        user = cursor.fetchone()
+        cursor.close()
+        conn.close()
+
+        if user:
+            session['username'] = user['fullName']
+            session['email'] = user['email']
+            session['address'] = user['address']
+            return redirect(url_for('index'))
+        else:
+            return render_template('login.html', error='Sorry Invalid email or password')
+
+    return render_template('login.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        name = request.form['fullName']
+        address = request.form['address']
+        email = request.form['email']
+        password = request.form['password']
+
+        # Check if the name is valid
+        if not is_valid_name(name):
+            return render_template('signup.html', error='Invalid name. Please enter a valid name.')
+
+        # Check if email already exists
+        conn = mysql.connector.connect(**mysql_config)
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM users WHERE email = %s", (email,))
+        user = cursor.fetchone()
+        if user:
+            cursor.close()
+            conn.close()
+            return render_template('signup.html', error='Sorry!. his email already exists')
+
+        # Insert user into database
+        cursor.execute("INSERT INTO users (fullName, address, email, password) VALUES (%s, %s, %s, %s)", (name, address,email, password))
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+        # session['username'] = name
+        return redirect(url_for('login'))
+    return render_template('signup.html')
+
+def is_valid_name(name):
+    # Add your validation logic here, for example:
+    # Name should contain only alphabets and spaces
+    return bool(re.match("^[a-zA-Z ]+$", name))
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('login'))
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    if 'username' not in session:
+        return redirect(url_for('login'))
     # Get input values from the form
     features = [float(request.form[feature]) for feature in ['sex', 'Age', 'Height', 'Weight', 'hypertension', 'diabetes']]
 
     # Make predictions using the models
     bmi_prediction = decision_tree_regression_model.predict([features])[0]
-    fitness_recommendation = decision_tree_classification__model.predict([features])[0]
+    fitness_recommendation = decision_tree_classification_model.predict([features])[0]
 
-    # Pass the predictions to the result template
-    return render_template('result.html', bmi_prediction=bmi_prediction, fitness_recommendation=fitness_recommendation)
+    # Convert NumPy arrays to Python lists
+    bmi_prediction_list = bmi_prediction.tolist()
+    fitness_recommendation_list = fitness_recommendation.tolist()
+
+    # Check if prediction exists
+    if bmi_prediction_list is not None and fitness_recommendation_list is not None:
+        # Return prediction results as JSON data
+        return jsonify(username = session['username'], email = session['email'], address= session['address'], bmi_prediction=bmi_prediction_list, fitness_recommendation=fitness_recommendation_list)
+    else:
+        # Return error if prediction failed
+        return jsonify(error="Prediction failed"), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/main.js
+++ static/js/main.js
@@ -1,169 +1,202 @@
-let burger = document.querySelector(".fa-bars");
-let navigationBar = document.querySelector(".navigation-bar");
-burger.addEventListener("click", ()=>{
-    navigationBar.classList.toggle("active")
-})
+//let burger = document.querySelector(".fa-bars");
+//let navigationBar = document.querySelector(".navigation-bar");
+//burger.addEventListener("click", () => {
+//    navigationBar.classList.toggle("active");
+//});
 
-var age = document.querySelector("#age");
-var height = document.querySelector("#height");
-var weight = document.querySelector("#weight");
-var sex = document.querySelector("#sex");
-var form = document.querySelector("#form");
-var result = "";
-form.addEventListener('submit', (event) => {
+$('#form').submit(function(event) {
     event.preventDefault();
-    if(age.value === ""){
-         const Toast = Swal.mixin({
-            toast: true,
-            position: "top-right",
-            showConfirmButton: false,
-            timer: 2000,
-            timerProgressBar: true,
-            didOpen: (toast) => {
-              toast.onmouseenter = Swal.stopTimer;
-              toast.onmouseleave = Swal.resumeTimer;
-            }
-          });
-          Toast.fire({
-            icon: "error",
-            title: "Age is required please!"
-          });
+    // Get form data
+    var age = $("#age").val();
+    var height = $("#height").val();
+    var weight = $("#weight").val();
+
+    // Perform form validation
+    if (age === "") {
+        showErrorToast("Age is required please!");
         return;
-    }
-    else if(age.value < 14){
-        const Toast = Swal.mixin({
-            toast: true,
-            position: "top-right",
-            showConfirmButton: false,
-            timer: 2000,
-            timerProgressBar: true,
-            didOpen: (toast) => {
-              toast.onmouseenter = Swal.stopTimer;
-              toast.onmouseleave = Swal.resumeTimer;
-            }
-          });
-          Toast.fire({
-            icon: "error",
-            title: "Age must be range (14-63)"
-          });
+    } else if (age < 14 || age > 63) {
+        showErrorToast("Age must be between 14 and 63");
         return;
-    }
-    else if(age.value < 1){
-        const Toast = Swal.mixin({
-            toast: true,
-            position: "top-right",
-            showConfirmButton: false,
-            timer: 2000,
-            timerProgressBar: true,
-            didOpen: (toast) => {
-              toast.onmouseenter = Swal.stopTimer;
-              toast.onmouseleave = Swal.resumeTimer;
-            }
-          });
-          Toast.fire({
-            icon: "error",
-            title: "Invalid Age. Please enter valid age!"
-          });
+    } else if (height === "") {
+        showErrorToast("Height is required please!");
+        return;
+    } else if (height < 1 || height > 2) {
+        showErrorToast("Please enter a valid height (between 1 and 2 meters)");
+        return;
+    } else if (weight === "") {
+        showErrorToast("Weight is required please!");
+        return;
+    } else if (weight < 30) {
+        showErrorToast("Sorry, you cannot use this system because your weight is less than 30 kg");
         return;
     }
 
-      else if(height.value === ""){
-        const Toast = Swal.mixin({
-            toast: true,
-            position: "top-right",
-            showConfirmButton: false,
-            timer: 2000,
-            timerProgressBar: true,
-            didOpen: (toast) => {
-              toast.onmouseenter = Swal.stopTimer;
-              toast.onmouseleave = Swal.resumeTimer;
-            }
-          });
-          Toast.fire({
-            icon: "error",
-            title: "Height is required please!"
-          });
-        return;
-    }
-      else if(height.value < 1){
-        const Toast = Swal.mixin({
-            toast: true,
-            position: "top-right",
-            showConfirmButton: false,
-            timer: 2000,
-            timerProgressBar: true,
-            didOpen: (toast) => {
-              toast.onmouseenter = Swal.stopTimer;
-              toast.onmouseleave = Swal.resumeTimer;
-            }
-          });
-          Toast.fire({
-            icon: "error",
-            title: "Impossible height please enter valid height!"
-          });
-        return;
-    }
-    else if(height.value > 2){
-        const Toast = Swal.mixin({
-            toast: true,
-            position: "top-right",
-            showConfirmButton: false,
-            timer: 2000,
-            timerProgressBar: true,
-            didOpen: (toast) => {
-              toast.onmouseenter = Swal.stopTimer;
-              toast.onmouseleave = Swal.resumeTimer;
-            }
-          });
-          Toast.fire({
-            icon: "error",
-            title: "Impossible height please enter valid height! (Mater)"
-          });
-        return;
-    }
-      else if(weight.value === ""){
-        const Toast = Swal.mixin({
-            toast: true,
-            position: "top-right",
-            showConfirmButton: false,
-            timer: 2000,
-            timerProgressBar: true,
-            didOpen: (toast) => {
-              toast.onmouseenter = Swal.stopTimer;
-              toast.onmouseleave = Swal.resumeTimer;
-            }
-          });
-          Toast.fire({
-            icon: "error",
-            title: "Weight is required Please!)"
-          });
-        return;
-    }
-      else if(weight.value < 30){
-        const Toast = Swal.mixin({
-            toast: true,
-            position: "top-right",
-            showConfirmButton: false,
-            timer: 2000,
-            timerProgressBar: true,
-            didOpen: (toast) => {
-              toast.onmouseenter = Swal.stopTimer;
-              toast.onmouseleave = Swal.resumeTimer;
-            }
-          });
-          Toast.fire({
-            icon: "error",
-            title: "Sorry You cannot use this system because your less then 30"
-          });
-        return;
-    }else{
-        // Set the form action and method
-    form.action = "/predict";
-    form.method = "POST";
+    // If all validations pass, submit the form via AJAX
+    $.ajax({
+        type: 'POST',
+        url: '/predict',
+        data: $('#form').serialize(),
+        success: function(response) {
+            // Display the recommendation
+            var html = `
+             <div class="container-fluid p-4" style="background-color: #A75DB4; min-height: 80vh;">
+                <h2 class="fw-bold p-2 display-5 text-center text-light">Report</h2>
+             <div class="container p-2" style="background-color: #fff;">
 
-    // Submit the form
-    form.submit();
-    }
-})
+        <div class="row">
+             <div class="col-sm-12">
+
+                                <div class="card" style="min-height:100vh;">
+                                <div class="table-responsive p-1"  id="printArea">
+
+                                    <table class="table" style="font-size: 15px;">
+                                        <tbody>
+                                            <tr>
+                                                <td class="fw-bold">Your Name:</td>
+                                                <td>${response.username}</td>
+                                            </tr>
+                                            <tr>
+                                                <td class="fw-bold">Your Address:</td>
+                                                <td>${response.address}</td>
+                                            </tr>
+                                            <tr>
+                                                <td class="fw-bold">Your Email:</td>
+                                                <td>${response.email}</td>
+                                            </tr>
+                                            <tr>
+                                                <td class="fw-bold">Your BMI: </td>
+                                                <td id="bmi"> ${response.bmi_prediction[0].toFixed(2)}</td>
+                                            </tr>
+                                            <tr>
+                                                <td class="fw-bold">Your Standard Weight: </td>
+                                                <td> ${response.bmi_prediction[1].toFixed(2)} (kg) -  ${response.bmi_prediction[2].toFixed(2) } (kg)</td>
+                                            </tr>
+                                            <tr>
+                                                <td class="fw-bold">Your Level:</td>
+                                                <td>${response.fitness_recommendation[0] }</td>
+                                            </tr>
+                                            <tr>
+                                                <td class="fw-bold">Fat Percentage:</td>
+                                                <td> ${response.bmi_prediction[3].toFixed(2) } </td>
+                                            </tr>
+                                            <tr>
+                                                <td class="fw-bold">Your Fitness Gaol:</td>
+                                                <td> ${response.fitness_recommendation[1]}</td>
+                                            </tr>
+                                            <tr>
+                                                <td class="fw-bold">Your Fitness Type:</td>
+                                                <td>${response.fitness_recommendation[2] }</td>
+                                            </tr>
+                                            <tr>
+                                                <td class="fw-bold">Exercises: </td>
+                                                <td>${response.fitness_recommendation[3] }</td>
+                                            </tr>
+
+                                            <tr>
+                                                <td class="fw-bold">Equipment needed:</td>
+                                                <td> ${response.fitness_recommendation[4] }</td>
+                                            </tr>
+                                            <tr>
+                                                <td class="fw-bold">Diet:</td>
+                                                <td>${response.fitness_recommendation[5] }</td>
+                                            </tr>
+                                            <tr>
+                                                <td class="fw-bold">Conclusion and Recommendation: </td>
+                                                <td>${response.fitness_recommendation[6] }</td>
+                                            </tr>
 
 
+                                        </tbody>
+                                    </table>
+                                </div>
+
+                                </div>
+                 <div class="mt-2 mb-2">
+                     <button class="btn btn-success text-light" id="printStatement"><i class="fas fa-print text-light"></i> Print</button>
+                     <button class="btn btn-info text-light" id="exportStatement"><i class="fas fa-file-excel text-light"></i> Export </button>
+                 </div>
+                            </div>
+        </div>
+        </div>
+    </div>
+            `;
+           $('#result').html(html);
+        },
+        error: function(xhr, status, error) {
+            // Handle error response
+            showErrorToast("Error occurred during prediction: " + error);
+        }
+    });
+});
+
+function showErrorToast(message) {
+    const Toast = Swal.mixin({
+        toast: true,
+        position: "top-right",
+        showConfirmButton: false,
+        timer: 2000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.onmouseenter = Swal.stopTimer;
+            toast.onmouseleave = Swal.resumeTimer;
+        }
+    });
+    Toast.fire({
+        icon: "error",
+        title: message
+    });
+}
+
+$(document).on("click", "#printStatement", function(){
+    printStatement();
+});
+
+$(document).on("click", "#exportStatement", function(event) {
+    var file = new Blob([$("#printArea").html()], {type: "text/html"});
+    var url = URL.createObjectURL(file);
+    var a = $("<a />", {
+        href: url,
+        download: "Personal_Recommendation.xls"
+    }).appendTo("body").get(0).click();
+    event.preventDefault();
+});
+
+
+function printStatement(){
+    let printArea = document.querySelector("#printArea");
+
+    var newWindow = window.open("");
+    newWindow.document.write(`<html><head><title>Report Recommendation Information</title>`);
+    newWindow.document.write(`<style media="print">
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap');
+    body{
+        font-family: 'Poppins', sans-serif;
+    }
+    table{
+        width: 100%;
+
+    }
+    h1{
+        font-size: 22 !important;
+        padding: 15px !important;
+    }
+    td.fw-bold{
+//        background-color: #A75DB4 !important;
+        color: #000 !important;
+        font-size: 16px !important;
+        font-weight: bold !important;
+    }
+    th , td{
+        padding: 15px !important;
+        text-align: left !important;
+        border-bottom: 1px solid #ddd !important;
+    }
+    </style>`)
+    newWindow.document.write(`</head><body>`)
+    newWindow.document.write(printArea.innerHTML);
+    newWindow.document.write(`</body></html>`);
+    newWindow.print();
+   newWindow.close();
+
+}
